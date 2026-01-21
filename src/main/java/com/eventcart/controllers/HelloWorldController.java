22d8d963fc993ffd387ff31114454fa9c3d61da5
Vishal palla <vishalgate2019@gmail.com>
2026-01-21 08:54:54 +0530

    aws secret manager

--- src/main/java/com/eventcart/controllers/HelloWorldController.java
+++ src/main/java/com/eventcart/controllers/HelloWorldController.java
@@ -12,7 +12,7 @@ import lombok.extern.log4j.Log4j2;
 public class HelloWorldController {
 	
 	
-	@Value("${DB_HOST}")
+	@Value("${DB_HOST:NOT_SET}")
 	private String DB_HOST;
 	
 	@GetMapping(path = "message")
