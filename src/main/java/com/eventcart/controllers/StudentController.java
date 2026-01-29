c93d65eb8a9753431b6842227db48b27b8357534
Vishal palla <vishalgate2019@gmail.com>
2026-01-29 22:13:50 +0530

    added an new api

--- src/main/java/com/eventcart/controllers/StudentController.java
+++ src/main/java/com/eventcart/controllers/StudentController.java
@@ -1,10 +1,12 @@
 package com.eventcart.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +18,7 @@ import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @RestController
-public class HelloWorldController {
+public class StudentController {
 
 	@Autowired
 	private StudentRepository studentRepository;
@@ -32,6 +34,13 @@ public class HelloWorldController {
 	public List<Student> getStudents() {
 
 		return studentRepository.findAll();
+
+	}
+	
+	@GetMapping("/students/{id}")
+	public Optional<Student> getStudents(@PathVariable("id") long id) {
+
+		return studentRepository.findById(id);
 
 	}
 
